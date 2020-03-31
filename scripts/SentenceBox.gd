68339ac1a3f07b2d67aaa44ba402546369387e78
Carles <carles@laptop-carles.laptop-carles>
2020-03-31 19:38:36 +0200

    Implemented hint button

--- scripts/SentenceBox.gd
+++ scripts/SentenceBox.gd
@@ -4,6 +4,7 @@ signal done
 
 onready var original_label = find_node("OriginalSentence")
 onready var translation_box = find_node("UserTranslation")
+onready var hintbutton = find_node("HintButton")
 
 var original = ""
 var translation = ""
@@ -15,7 +16,7 @@ func set_sentence(_original, _translation, done=false):
 	original = _original
 	translation = _translation
 	original_label.text = translation
-	translation_box.rect_min_size.y = original_label.rect_size.y+5
+	translation_box.rect_size.y = original_label.rect_size.y
 	
 	if done:
 		translation_box.text = original
@@ -25,6 +26,7 @@ func _on_text_changed():
 	var user_translation = translation_box.text
 	if user_translation == "":
 		find_node("Indicator").text = ""
+		
 	elif user_translation == original:
 		on_completion()
 	else:
@@ -36,6 +38,7 @@ func _on_text_changed():
 func on_completion():
 		find_node("Indicator").text = "Done!"
 		translation_box.readonly = true
+		hintbutton.disabled = true
 		done = true
 		emit_signal("done")
 		if next_sentence_box != null:
@@ -47,3 +50,13 @@ func grab_focus():
 func set_focus_next(_next_sentence_box):
 	next_sentence_box = _next_sentence_box
 	translation_box.set_focus_next(next_sentence_box.get_path())
+
+
+func _on_HintButton_pressed():
+#	var user_translation = translation_box.text
+	var correct = 0
+	for c in translation_box.text:
+		if c == original[correct]:
+			correct += 1
+	translation_box.text = original.substr(0, correct+1)
+
