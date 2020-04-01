1c8c154f282a6323c1bf7f6d120f91ec2f16f514
Carles <carles@laptop-carles.laptop-carles>
2020-04-01 21:50:57 +0200

    Subsituted text buttons for flags when choosing language

--- scripts/LanguageMenu.gd
+++ scripts/LanguageMenu.gd
@@ -11,15 +11,22 @@ const LANGUAGES = {"de":"Deutsch",
 				   "es":"Spanish"}
 					
 onready var language_list = find_node("LanguageList")
+onready var language_grid = find_node("LanguageGrid")
 
 func _ready():
 	for lang in LANGUAGES:
-		var new_lb = LanguageBox.instance()
-
-		language_list.add_child(new_lb)
-		new_lb.text = LANGUAGES[lang]
-		new_lb.connect("pressed", self,  "_on_language_selected", [lang])
+#		var new_lb = LanguageBox.instance()
+#		new_lb.text = LANGUAGES[lang]
 		
+		var new_lb = TextureButton.new()
+#		var lang_texture = StreamTexture.new()
+#		lang_texture.load_path = "res://flags/%s.png" % lang
+		var lang_texture = load("res://flags/%s.png" % lang)
+		new_lb.texture_normal = lang_texture
+#		new_lb.texture_normal = lang_texture
+
+		language_grid.add_child(new_lb)
+		new_lb.connect("pressed", self,  "_on_language_selected", [lang])
 			
 func _on_language_selected(lang):
 	emit_signal("language_selected", lang)
